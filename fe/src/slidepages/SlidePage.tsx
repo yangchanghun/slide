4103454928d27a29c7d2d44a45f7ba0620e8e4d9
yangchanghun <gory4848@naver.com>
2026-04-08 13:37:15 +0900

    변경

--- fe/src/slidepages/SlidePage.tsx
+++ fe/src/slidepages/SlidePage.tsx
@@ -4,20 +4,22 @@ export const SlidePage = () => {
   const navigate = useNavigate();
   return (
     <div className="flex min-h-screen items-center justify-center text-lg">
-      <button
-        onClick={() => {
-          navigate("/slider/1");
-        }}
-      >
-        합정초등학교
-      </button>
-      <button
-        onClick={() => {
-          navigate("/slider/2");
-        }}
-      >
-        수정초등학교
-      </button>
+      <div className="flex flex-col">
+        <button
+          onClick={() => {
+            navigate("/slider/1");
+          }}
+        >
+          합정초등학교
+        </button>
+        <button
+          onClick={() => {
+            navigate("/slider/2");
+          }}
+        >
+          수정초등학교
+        </button>
+      </div>
     </div>
   );
 };
